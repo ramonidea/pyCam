0b3ea923d4acf3c2d4fb2722eae385d393b8457a
ramonidea <quramon@gmail.com>
2018-01-25 18:54:40 -0800

    subscriber tested scipy zoom function

--- pyConn/subscriber.py
+++ pyConn/subscriber.py
@@ -5,35 +5,28 @@ import numpy as np
 import rospy
 from std_msgs.msg import String
 import blosc
+from scipy.ndimage.interpolation import zoom
 
 count = 0
 lasttime = 0
 
-r = np.zeros((480*640,1),dtype=np.uint8)
-g = np.zeros((480*640,1),dtype=np.uint8)
-b = np.zeros((480*640,1),dtype=np.uint8)
-d = np.zeros((480*640),dtype=np.uint8)
-
-rb = False
-gb = False
-bb = False
-db = False
 
 
 def parseData(data):
     #print(len(data))
     data = blosc.decompress(data)
-    #data = np.split(data,[921600])
+    #data = np.split(data,[230400])
 
-    data = np.fromstring(data, dtype = np.uint8).reshape(480,640)
-    #color = np.fromstring(data[0], dtype=np.uint8).reshape(480,640,3)
-    #depth = np.fromstring(data[1], dtype=np.uint8).reshape(480,640)
-    #d4d = 255 - cv2.cvtColor(depth, cv2.COLOR_GRAY2RGB)
+    #data = np.fromstring(data, dtype = np.uint8).reshape(240,320)
+    #color = zoom(np.fromstring(data[0], dtype=np.uint8).reshape(240,320,3), [2,2,1])
+    depth = zoom(np.fromstring(data, dtype=np.uint8).reshape(240,320),[2,2])
+
+    d4d = 255 - cv2.cvtColor(depth, cv2.COLOR_GRAY2RGB)
 
     #d4d = frame
     #d4d = np.uint8(frame.astype(float) * 255 / 2 ** 12 - 1)
-    return data
-    #return np.hstack((color,d4d))
+    #return data
+    return d4d
 
 
 def callback(data):
@@ -41,18 +34,16 @@ def callback(data):
 
     global count, lasttime
     #parseData(data.data)
-    #cv2.waitKey(1) & 255
-    #cv2.imshow("RGBD", parseData(data.data))
-    print(len(parseData(data.data)))
+    cv2.waitKey(1) & 255
+    cv2.imshow("RGBD", parseData(data.data))
+    #print(len(parseData(data.data)))
 
-
-'''
     if (int(round(time.time() * 1000)) - lasttime > 5000):
         lasttime = int(round(time.time() * 1000))
         print("Average FPS:" + str(count / 5.0))
         count = 0
     count += 1
-    '''
+
 
 def listener():
     global count, lasttime
@@ -60,7 +51,7 @@ def listener():
     rospy.init_node('Ramon', anonymous=False)
     print("Finish initialize ROS node")
     lasttime = int(round(time.time() * 1000))
-    rospy.Subscriber("/RGBC", String, callback)
+    rospy.Subscriber("/RGBD", String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
