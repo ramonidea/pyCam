549a91d37d75142194f9060d87a8a648db6a24aa
ramonidea <quramon@gmail.com>
2018-02-01 13:58:15 -0800

    update laptop side

--- pureConn/client.py
+++ pureConn/client.py
@@ -6,14 +6,17 @@ Pure Python Socket program, without using ROS platform
 Purpose: To test the transmission rate and latency vs runing on ROS
 '''
 import cv2
-import socket
+from socket import *
 import base64
 import numpy as np
 import blosc
 import time
+import zlib
+from PIL import Image
+import StringIO
 
 IP_SERVER = "69.91.157.166"
-PORT_SERVER = 1080
+PORT_SERVER = 2005
 TIMEOUT_SOCKET = 10
 SIZE_PACKAGE = 4096
 
@@ -23,32 +26,49 @@ COLOR_PIXEL = 3  # RGB
 
 
 if __name__ == '__main__':
-    PORT = 5000  # default 5000 for both sides
+    PORT = 3030  # default 5000 for both sides
+    s = socket(AF_INET, TCP_NODELAY)
     s.bind(('', PORT))
     s.listen(5)
-    BUFSIZE = 8192  # should be 2^n
-    s = socket(AF_INET, TCP_NODELAY)
+    BUFSIZE = 300000  # should be 2^n
+
 
     count = 0
     lasttime = int(round(time.time() * 1000))
 
     while True:
         try:
-            conn, (host, remoteport) = self.s.accept()
+            conn, (host, remoteport) = s.accept()
             arr1 = b""
             while True:
-                data = conn.recv(self.BUFSIZE)
+                data = conn.recv(BUFSIZE)
                 if not data:
                     break
                 arr1 += data
 
-            data = blosc.unpack_array(result)
-            #data = np.fromstring(result, dtype=np.uint8).reshape(480,640,4)
-            data = np.dsplit(data,[3])
-            color = data[0]
-            depth = data[1].reshape(480,640)
-            d4d = 255 - cv2.cvtColor(depth, cv2.COLOR_GRAY2RGB)
-            cv2.imshow('Depth || Color', np.hstack((rgb,d4d)))
+            #data = zlib.decompress(arr1)
+
+            #data = np.fromstring(data, dtype=np.uint8).reshape(240,320,3)
+            rgb = Image.open(StringIO.StringIO(arr1))
+            rgb = np.array(rgb)
+
+            conn, (host, remoteport) = s.accept()
+            arr1 = b""
+            while True:
+                data = conn.recv(BUFSIZE)
+                if not data:
+                    break
+                arr1 += data
+            depth = Image.open(StringIO.StringIO(arr1))
+            depth = np.array(depth)
+            #print(rgb.shape)
+            cv2.imshow('Color', np.hstack((rgb,depth)))
+            #data = np.fromstring(data, dtype=np.uint8).reshape(240,320,4)
+            #data = np.dsplit(data,[3])
+            #rgb = data[0]
+            #depth = data[1].reshape(240,320)
+            #d4d = 255 - cv2.cvtColor(depth, cv2.COLOR_GRAY2RGB)
+            #cv2.imshow('Depth || Color', np.hstack((rgb,d4d)))
 
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
