ac8eee334c0a62605a627d90b948994e159656be
CoMotion Admin <comotion@D-69-91-159-155.dhcp4.washington.edu>
2018-02-16 12:16:35 -0800

    Fix the get frame Function

--- pyVideo/camera.py
+++ pyVideo/camera.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import cv2
 from openni2_device_init import visionsensor
 import numpy as np
@@ -12,19 +13,26 @@ class VideoCamera(object):
             depth_mirror = False, rgb = True, depth = True):
         self.device = visionsensor(x = x, y = y, fpd = fps, rgb_mirror = rgb_mirror,
                 depth_mirror = depth_mirror, rgb = rgb, depth = depth)
+        self.rgb = rgb
+        self.depth = depth
         time.sleep(1)
 
 
     def __del__(self):
         self.device.stop()
 
-    def get_frame(self):
-        rgb = self.device.getRgb()
-        #img = Image.fromarray(rgb)
-        #fpath =BytesIO()
-        #img.save(fpath, quality = 75, format = "JPEG")
-        #fpath.seek(0)
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 75]
-        ret, jpeg = cv2.imencode('.jpg', rgb, encode_param)
-        depth = self.device.getDepth()
-        return jpeg.tobytes() , zlib.compress(depth)
+    def get_frame(self, rgb_compress = 75):
+        jpeg = ''
+        depth = ''
+        if self.rgb:
+            rgb = self.device.getRgb()
+            #img = Image.fromarray(rgb)
+            #fpath =BytesIO()
+            #img.save(fpath, quality = 75, format = "JPEG")
+            #fpath.seek(0)
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), rgb_compress]
+            ret, jpeg = cv2.imencode('.jpg', rgb, encode_param)
+            jpeg = jpeg.tobytes()
+        if self.depth:
+            depth = zlib.compress(self.device.getDepth())
+        return jpeg, depth
