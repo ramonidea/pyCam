dff8c07c9c36aa92fa08ab112d976cd79ffb4ab7
CoMotion Admin <comotion@D-69-91-159-155.dhcp4.washington.edu>
2018-02-16 14:12:43 -0800

    Add ROS publish part

--- pyVideo/receiver.py
+++ pyVideo/receiver.py
@@ -9,6 +9,18 @@ import cv2
 from io import BytesIO
 import time
 import zlib
+import rospy
+from sensor_msgs.msg import CompressedImage,CameraInfo
+from std_msgs.msg import String
+
+#To parse the command line arguments
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
 
 def getFrame(response):
     rgbData = b''
@@ -32,9 +44,69 @@ def getFrame(response):
     depthData = response.read(int(depth))
     return BytesIO(rgbData), zlib.decompress(depthData)
 
+def retriveCameraInfo(ip, port):
+    info = urlopen("http://"+ip+':'+port+'/camera_info')
+    result = str(info.read(1024)).split('-')
+    x = int(result[0][1:])
+    y = int(result[1][1:])
+    fps = int(result[2][3:])
+    return x,y,fps
+
 
 if __name__ == '__main__':
-    video = urlopen("http://173.250.181.115:5000/video_feed")
+    #Global Variables
+
+    ip = ''
+    port = ''
+    videoX = 640
+    videoY = 480
+    videoFps = 30
+    rgb = True
+    depth = True
+
+    myargs = getopts(argv)
+    try:
+        if "-ip" in myargs:
+            ip = myargs["-ip"]
+        if "-port" in myargs:
+            port = myargs["-port"]
+    except Exception, e:
+        print(e.message)
+
+    if ip == "" or port == '':
+        print("Please Enter IP Address of the local machine")
+    else:
+        print("Remote Joule is set to "+ip+':' +str(port))
+        print("Init ROS")
+        rgb_pub = rospy.Publisher("/Camera/rgb",CompressedImage,queue_size=30)
+        print("/Camera/rgb is published, (CompressedImage)")
+        depth_pub = rospy.Publisher("/Camera/depth",CompressedImage,queue_size=30)
+        print("/Camera/depth is published, (CompressedImage)")
+        camera_info = rospy.Publisher("/Camera/camera_info",CameraInfo,queue_size=30)
+        print("/Camera/camera_info is published, (camera_info)")
+        rospy.init_node("Joule", anonymous=False)
+         while not rospy.is_shutdown():
+            try:
+                if camera_info.get_num_connections() > 0:
+                    videoX, videoY, videoFps = retriveCameraInfo(ip,port)
+
+
+                if depth_pub.get_num_connections() > 0 or rgb_pub.get_num_connections() > 0:
+                    video = urlopen("http://"+ip+":"+port+"/video_feed")
+                    while depth_pub.get_num_connections() > 0 or rgb_pub.get_num_connections() > 0:
+                        rgb,depth = getFrame(video)
+                        rgb = cv2.cvtColor(np.asarray(Image.open(rgb)), cv2.COLOR_RGB2BGR)
+
+                        depth = np.fromstring(depth,dtype=np.uint16).reshape(480,640)
+
+
+            except Exception,e:
+                if e == KeyboardInterrupt:
+                    break
+
+
+
+
     count = 0
     lasttime = 0
     lasttime = int(round(time.time() * 1000))
