9f21b9e85c70c941e1499576a58d469c1f4b002e
Ramon Qu <quramon@gmail.com>
2018-02-23 13:43:26 -0800

    init wrapper

--- realsense_wrapper/realsense_device.py
+++ realsense_wrapper/realsense_device.py
@@ -14,48 +14,46 @@
 # sudo make install #Optional if you want the library to be installed in your system
 #
 #
-import pyrealsense2 as rs
-import numpy as np
-import cv2
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+class visionsensor:
+    def __init__(self, x = 640, y = 480, fpd = 30, rgb = True, depth = True):
+        self.x = x
+        self.y = y
+        self.fps = fps
+        self.rgb = rgb
+        self.depth = depth
 
-# Start streaming
-pipeline.start(config)
 
-try:
-    while True:
+    #Start the Color Camera
+    def startColor(self):
 
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
 
-        # Convert images to numpy arrays
-        depth_image = 255 - cv2.cvtColor(np.asarray(depth_frame.get_data()), cv2.COLOR_GRAY2RGB)
-        #depth_image = np.asarray(depth_frame.get_data())
+    #Start the Depth Camera
+    def startDepth(self):
 
-        color_image = np.asarray(color_frame.get_data())
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    #Stop the Depth Camera
+    def stop(self):
 
-        # Stack both images horizontally
-        images = np.hstack((color_image, depth_image))
-        print(images.shape)
 
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+    #Initialize color camera (default 640 * 360 * 30fps)
+    def createColor(self):
 
-finally:
 
-    # Stop streaming
-    pipeline.stop()
+    #Initialize the Depth camera (default 640*360*30fps)
+    def createDepth(self):
+
+    #Enable the depth and color sync (un after initalized both cameras, before running them)
+    def sync(self):
+
+    #Return / Initalize self.rgb as the numpy array (uint8 3L 640 * 480 * 3 * uint8)
+    def getRgb(self):
+
+    #Return self.dmap as the numpy array (1L uint16, 0 - 2**12-1)
+    def getDepth(self):
+
+
+
+
+
+    def getDepth2Int8(self):
