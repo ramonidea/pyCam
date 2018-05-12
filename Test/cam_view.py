ab2e25bb7bb581f3a0371cbed552376ca41b5446
ramonidea <quramon@gmail.com>
2018-05-11 18:57:00 -0700

    redo the tests

--- Test/cam_view.py
+++ Test/cam_view.py
@@ -47,7 +47,7 @@ def parseData(data):
 
 def ShowCompressedImage(data):
     np_arr = np.fromstring(data.data, np.uint8)
-
+    #print(np_arr.shape)
     image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     return image_np
 
@@ -69,6 +69,7 @@ def ShowDepth(data):
          print(e)
 def ShowCompressedDepth(data):
     np_arr = np.fromstring(data.data, np.uint8)
+
     image_np = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
 
     return image_np
@@ -76,12 +77,13 @@ def ShowCompressedDepth(data):
 def callback(data):
     global count, lasttime,latency,fps,lat
     #rospy.loginfo(rospy.get_caller_id() + "I heard")
-    cv2.waitKey(1) & 255
-    cv2.imshow("Color", ShowCompressedImage(data))
+    #cv2.waitKey(1) & 255
+    #cv2.imshow("Color", ShowCompressedImage(data))
     #ShowCompressedImage(data)
-
+    ShowImage(data)
     timestamp = data.header.stamp
-    latency += int(time.time()) - timestamp.secs
+    latency += time.time() - (timestamp.secs+round((timestamp.nsecs/ 1000000000.0),5))
+
     if (int(round(time.time() * 1000)) - lasttime > 5000):
         lasttime = int(round(time.time() * 1000))
         print("Average C FPS:" + str(count / 5.0))
@@ -90,10 +92,11 @@ def callback(data):
         lat.append(latency * 1.0 / count)
         latency = 0
         count = 0
-    if(len(fps) > 100 ):
-        with open("comfps.json","wb") as outfile:
+
+    if(len(fps) > 100):
+        with open("resultfps.json","wb") as outfile:
             json.dump(fps,outfile)
-        with open("comlat.json","wb") as outfile:
+        with open("resultlat.json","wb") as outfile:
             json.dump(lat, outfile)
         print("Save C to File")
         fps = []
@@ -103,12 +106,12 @@ def callback(data):
 def callback1(data):
     global count1, lasttime1,latency1,fps1,lat1
     #rospy.loginfo(rospy.get_caller_id() + "I heard")
-    cv2.waitKey(1) & 255
-    cv2.imshow("Depth", ShowCompressedDepth(data))
+    #cv2.waitKey(1) & 255
+    #cv2.imshow("Depth", ShowCompressedDepth(data))
     #ShowCompressedDepth(data)
-
+    ShowDepth(data)
     timestamp = data.header.stamp
-    latency1 += int(time.time()) - timestamp.secs
+    latency1 += time.time() - (timestamp.secs+round((timestamp.nsecs/ 1000000000.0),5))
     if (int(round(time.time() * 1000)) - lasttime1 > 5000):
         lasttime1 = int(round(time.time() * 1000))
         print("Average D FPS:" + str(count1 / 5.0))
@@ -118,9 +121,9 @@ def callback1(data):
         latency1 = 0
         count1 = 0
     if(len(fps1) > 100 ):
-        with open("comfps1.json","wb") as outfile:
+        with open("resultfps1.json","wb") as outfile:
             json.dump(fps1,outfile)
-        with open("comlat1.json","wb") as outfile:
+        with open("resultlat1.json","wb") as outfile:
             json.dump(lat1, outfile)
         print("Save D to File")
         fps1 = []
@@ -138,8 +141,8 @@ def listener():
     rospy.init_node('listener', anonymous=False)
     lasttime = int(round(time.time() * 1000))
     lasttime1 = int(round(time.time() * 1000))
-    #rospy.Subscriber("/camera/color/image_raw/compressed", CompressedImage, callback,  queue_size = 1)
-    rospy.Subscriber("/camera/depth/image_rect_raw/compressed", CompressedImage, callback1,  queue_size = 1)
+    rospy.Subscriber("/camera/color/image_raw", Image, callback,  queue_size = 1)
+    rospy.Subscriber("/camera/depth/image_rect_raw", Image, callback1,  queue_size = 1)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
