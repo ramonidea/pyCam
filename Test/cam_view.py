9a28c8018f451b777912319ecbdf15761f710a9c
ramonidea <quramon@gmail.com>
2018-05-20 14:57:55 -0700

    update the record function

--- Test/cam_view.py
+++ Test/cam_view.py
@@ -27,6 +27,11 @@ lasttime1 = 0
 fps1 = []
 lat1 = []
 
+# video recorder
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G') # cv2.VideoWriter_fourcc() does not exist
+video_writer = cv2.VideoWriter("output_2.avi", fourcc, 30, (640, 480))
+video_writer1 = cv2.VideoWriter("output_21.avi", fourcc, 30, (640, 480))
+
 #To parse the command line arguments
 def getopts(argv):
     opts = {}  # Empty dictionary to store key-value pairs.
@@ -35,7 +40,6 @@ def getopts(argv):
             opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
         argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
     return opts
-
 
 def parseData(data):
     #frame = zlib.decompress(data)
@@ -55,9 +59,11 @@ def ShowImage(data):
     global bridge
     try:
          cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+         print(cv_image.shape)
          return cv_image
     except CvBridgeError as e:
          print(e)
+
 
 def ShowDepth(data):
     global bridge
@@ -71,83 +77,66 @@ def ShowCompressedDepth(data):
     np_arr = np.fromstring(data.data, np.uint8)
 
     image_np = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
+    image_np =cv2.cvtColor(image_np, cv2.COLOR_GRAY2RGB)
 
     return image_np
 
 def callback(data):
-    global count, lasttime,latency,fps,lat
+    global count, lasttime,latency,fps,lat,video_writer,video_writer1
     #rospy.loginfo(rospy.get_caller_id() + "I heard")
-    #cv2.waitKey(1) & 255
-    #cv2.imshow("Color", ShowCompressedImage(data))
+    try:
+        #cv2.namedWindow("Color", cv2.WND_PROP_FULLSCREEN)
+        #cv2.setWindowProperty("Color", cv2.WND_PROP_FULLSCREEN, cv2.WND_PROP_FULLSCREEN)
+        cv2.waitKey(1) & 255
+        frame = ShowCompressedImage(data)
+        video_writer.write(frame)
+        cv2.imshow("Color", frame)
+    except:
+        pass
     #ShowCompressedImage(data)
-    ShowImage(data)
-    timestamp = data.header.stamp
-    latency += time.time() - (timestamp.secs+round((timestamp.nsecs/ 1000000000.0),5))
+    #ShowImage(data)
+    #timestamp = data.header.stamp
+    #latency += time.time() - (timestamp.secs+round((timestamp.nsecs/ 1000000000.0),5))
 
-    if (int(round(time.time() * 1000)) - lasttime > 5000):
-        lasttime = int(round(time.time() * 1000))
-        print("Average C FPS:" + str(count / 5.0))
-        print("Average C Latency:" + str(latency * 1.0 / count))
-        fps.append(count / 5.0)
-        lat.append(latency * 1.0 / count)
-        latency = 0
-        count = 0
 
-    if(len(fps) > 100):
-        with open("./uw_result(480)/resultfps.json","wb") as outfile:
-            json.dump(fps,outfile)
-        with open("./uw_result(480)/resultlat.json","wb") as outfile:
-            json.dump(lat, outfile)
-        print("Save C to File")
-        fps = []
-        lat = []
-    count+=1
 
 def callback1(data):
-    global count1, lasttime1,latency1,fps1,lat1
-    #rospy.loginfo(rospy.get_caller_id() + "I heard")
-    #cv2.waitKey(1) & 255
-    #cv2.imshow("Depth", ShowCompressedDepth(data))
-    #ShowCompressedDepth(data)
-    ShowDepth(data)
-    timestamp = data.header.stamp
-    temp = round(time.time(),5) - (timestamp.secs+round((timestamp.nsecs/ 1000000000.0),3))
-    if(temp >0):
-        latency1+=temp
-
-    if (int(round(time.time() * 1000)) - lasttime1 > 5000):
-        lasttime1 = int(round(time.time() * 1000))
-        print("Average D FPS:" + str(count1 / 5.0))
-        print("Average D Latency:" + str(latency1 * 1.0 / count1))
-        fps1.append(count1 / 5.0)
-        lat1.append(latency1 * 1.0 / count1)
-        latency1 = 0
-        count1 = 0
-    if(len(fps1) > 100 ):
-        with open("./uw_result(480)/resultfps1.json","wb") as outfile:
-            json.dump(fps1,outfile)
-        with open("./uw_result(480)/resultlat1.json","wb") as outfile:
-            json.dump(lat1, outfile)
-        print("Save D to File")
-        fps1 = []
-        lat1 = []
-    count1+=1
-
+    global count1, lasttime1,latency1,fps1,lat1,video_writer1,video_writer
+    try:
+            #cv2.namedWindow("Depth", cv2.WND_PROP_FULLSCREEN)
+            #cv2.setWindowProperty("Depth", cv2.WND_PROP_FULLSCREEN, cv2.WND_PROP_FULLSCREEN)
+            #cv2.waitKey(1) & 255
+            frame = ShowCompressedDepth(data)
+            video_writer1.write(frame)
+            #cv2.imshow("Depth", frame)
+    except:
+            pass
+        #rospy.loginfo(rospy.get_caller_id() + "I heard")
+        #cv2.waitKey(1) & 255
+        #cv2.imshow("Depth", ShowCompressedDepth(data))
+        #ShowCompressedDepth(data)
 
 def listener():
-    global count, lasttime,latency,bridge,lasttime1
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=False)
-    lasttime = int(round(time.time() * 1000))
-    lasttime1 = int(round(time.time() * 1000))
-    rospy.Subscriber("/camera/color/image_raw", Image, callback,  queue_size = 1)
-    rospy.Subscriber("/camera/depth/image_rect_raw", Image, callback1,  queue_size = 1)
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    global count, lasttime,latency,bridge,lasttime1, video_writer
+
+    try:
+
+        # In ROS, nodes are uniquely named. If two nodes with the same
+        # node are launched, the previous one is kicked off. The
+        # anonymous=True flag means that rospy will choose a unique
+        # name for our 'listener' node so that multiple listeners can
+        # run simultaneously.
+        rospy.init_node('listener', anonymous=False, disable_signals=True)
+        lasttime = int(round(time.time() * 1000))
+        lasttime1 = int(round(time.time() * 1000))
+        rospy.Subscriber("/camera/color/image_raw/compressed", CompressedImage, callback,  queue_size = 1)
+        rospy.Subscriber("/camera/depth/image_rect_raw/compressed", CompressedImage, callback1,  queue_size = 1)
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
+    except:
+                video_writer.release()
+                video_writer1.release()
+                cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
